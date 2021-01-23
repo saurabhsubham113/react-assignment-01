2db24e7caaaaa2adfcbb7524009cfd96994c20b0
subham saurabh <saurabhsubahm113@gmail.com>
2021-01-23 13:08:41 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(prevCounter => (
+      prevCounter + 1
+    ))
+  }
+
+  const decrementCounter = () => {
+    setCounter(prevCounter => (
+      prevCounter - 1
+    ))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter demo</h1>
+      <h3>Counter : {counter}</h3>
+      <button onClick={incrementCounter} >increment counter</button>
+      <button onClick={decrementCounter} >decrement counter</button>
+
     </div>
   );
 }
 
 export default App;
+
+
